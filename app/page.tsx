7f14c805bf1bd6d865200915396e0ab1b0617c92
selinanicol <selinanicol456@gmail.com>
2026-06-08 19:02:10 +0200

    fixing laounching problem

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Page() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -8,26 +8,30 @@ export default function Home() {
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
-          width={100}
-          height={20}
+          width={180}
+          height={38}
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            To get started, edit the{" "}
+            <code className="rounded-md bg-zinc-200/50 px-1.5 py-0.5 font-mono text-[0.9em] font-medium dark:bg-zinc-800/50">
+              app/page.tsx
+            </code>{" "}
+            file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              className="font-medium text-zinc-950 underline decoration-zinc-950/20 underline-offset-4 transition-colors hover:decoration-zinc-950/50 dark:text-zinc-50 dark:decoration-zinc-50/20 dark:hover:decoration-zinc-50/50"
             >
               Templates
             </a>{" "}
             or the{" "}
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              className="font-medium text-zinc-950 underline decoration-zinc-950/20 underline-offset-4 transition-colors hover:decoration-zinc-950/50 dark:text-zinc-50 dark:decoration-zinc-50/20 dark:hover:decoration-zinc-50/50"
             >
               Learning
             </a>{" "}
